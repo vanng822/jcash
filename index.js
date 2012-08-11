d488aa672a285bfc2b18dcb38909e46dad6ba72b
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2012-08-11 15:53:07 +0200

    dynamicHelpers from both image and css/js handlers will be merged into one

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var StaticHandler = require('./lib/static');
 var imageHandler = require('./lib/image');
 
-for (var proto in imageHandler) {
-	StaticHandler[proto] = imageHandler[proto];
-};
 
+function isObject(testObj) {
+	return typeof testObj == 'object';
+}
+
+function overrideProperty(obj, prop, value) {
+	if(!obj.hasOwnProperty(prop)) {
+		obj[prop] = value;
+		return;
+	}
+	/* deep override only when matching type object */
+	if(isObject(value) && isObject(obj[prop])) {
+		overrideObject(obj[prop], value);
+		return;
+	}
+	obj[prop] = value;
+}
+
+function overrideObject(obj) {
+	var i, len, source, j, jlen, props;
+	for( i = 1, len = arguments.length; i < len; i++) {
+		source = arguments[i];
+		props = Object.keys(source);
+		for( j = 0, jlen = props.length; j < jlen; j++) {
+			overrideProperty(obj, props[j], source[props[j]]);
+		}
+	}
+}
+
+overrideObject(StaticHandler, imageHandler);
 
 module.exports = StaticHandler;
